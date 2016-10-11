156a0214ac847b4e135277cf563890f7944b4a7e
Matheus Cendrão <matheuscendrao@gmail.com>
2016-10-10 22:19:37 -0300

    Refactor pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,11 @@ class PetsController < ApplicationController
   before_action :load_constants, only: [:new, :create]
 
   def index
-
-    if params[:ong_id]
-      fill_ong
-      @pets = @ong.pets
+    if params[:city]
+      @city = params[:city]
+      @pets = Pet.joins(:ong).where(ongs: { city: @city })
     else
-      if params[:city]
-        @pets = []
-        @city = params[:city]
-        Ong.where(city: params[:city]).each do |ong|
-          if ong.pets.present?
-            ong.pets.each do |pet|
-              @pets << pet
-            end
-          end
-        end
-      else
-        @pets = Pet.all
-      end
+      @pets = Pet.all
     end
   end
 
