053d0569b3f53a235e2332921a043dea501329e7
Flavio Avila <flavio.silva@vagas.com.br>
2017-05-01 17:43:25 -0300

    arrumado bug que mostrava botoes, bastanto estar logado em uma ong qualquer

--- spec/features/user_visits_ong_profile_spec.rb
+++ spec/features/user_visits_ong_profile_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 feature 'user visits ong profiles' do
-  scenario 'successfuly' do
+  scenario 'current user loged like ong' do
     ong = ong_login
     create(:pet, ong: ong)
-
     visit ong_path ong
 
     expect(page).to have_content ong.name
@@ -13,5 +12,38 @@ feature 'user visits ong profiles' do
     expect(page).to have_content ong.site
     expect(page).to have_content ong.address.city
     expect(page).to have_content ong.address.state
+
+    expect(page).to have_link 'Criar Evento'
+  end
+
+  scenario 'current user not loged like ong' do
+    ong = create(:ong)
+    create(:pet, ong: ong)
+    visit ong_path ong
+
+    expect(page).to have_content ong.name
+    expect(page).to have_content ong.phone
+    expect(page).to have_content ong.email
+    expect(page).to have_content ong.site
+    expect(page).to have_content ong.address.city
+    expect(page).to have_content ong.address.state
+
+    expect(page).to have_no_link 'Criar Evento'
+  end
+
+  scenario 'current user loged like ong, but on other page ong' do
+    ong = ong_login
+    ong_not_loged = create(:ong)
+    create(:pet, ong: ong)
+    visit ong_path ong_not_loged
+
+    expect(page).to have_content ong_not_loged.name
+    expect(page).to have_content ong_not_loged.phone
+    expect(page).to have_content ong_not_loged.email
+    expect(page).to have_content ong_not_loged.site
+    expect(page).to have_content ong_not_loged.address.city
+    expect(page).to have_content ong_not_loged.address.state
+
+    expect(page).to have_no_link 'Criar Evento'
   end
 end
