4406323aa1d0e33fc7873f247dccd2f068834a91
Flavio Avila <flavio.silva@vagas.com.br>
2017-07-11 17:01:51 -0300

    add conf paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,15 @@ Rails.application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
 end
