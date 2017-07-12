07f978aaf20bf845e38d781d6b270f00190ab175
Flavio Avila <flavio.silva@vagas.com.br>
2017-07-11 22:32:58 -0300

    conf aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,4 +3,4 @@ Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-sa-east-1.amazonaws.com'
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-sa-east-1.amazonaws.com'
