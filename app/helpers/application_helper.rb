5ab18c87097744bb5cf29ccfa9acef115c90d0bf
Flavio Avila <flavio.silva@vagas.com.br>
2017-07-22 01:11:16 -0300

    Melhoria no quem somo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def friendly_date(date)
     I18n.l(date, format: '%d/%m/%Y %H:%M')
   end
+
+  def ong_admin?(ong)
+    return false if ong.nil?
+    ong_signed_in? && current_ong.id == ong.id
+  end
+
+  def user_admin?(user)
+    return false if user.nil?
+    user_signed_in? && current_user.id == user.id
+  end
 end
